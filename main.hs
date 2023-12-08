96d379f5dbbb4d1139b7e5c18d37889b2435affd
Katya-Kuzenko <keyti.keuz@gmail.com>
2023-12-08 22:19:23 +0100

    init poject

---
+++ main.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE InstanceSigs #-}
+
+{-
+Dictionaries are one of the most common data structure.
+They are associative collections Dict k v
+indexed by key of type k with values of type v.
+
+Their inner structure can be different, but they have a common interface:
+
+insert       :: k -> v -> Dict k v -> Dict k v
+maybeGet     :: k -> Dict k v -> Maybe v
+getOrDefault :: k -> Dict k v -> v -> v
+contains     :: k -> Dict k v -> Bool
+delete       :: k -> Dict k v -> Dict k v
+elems        :: Dict k v -> [v]
+keys         :: Dict k v -> [k]
+size         :: Dict k v -> Int
+empty        :: Dict k v 
+-}
+
+
+-- 1. Populate a following typeclass
+--    d denotes the actual type of the dictionary (its type constructor)   
+class IDict d k v where
+  -- ???
+
+
+-- 2. Propose a naive implementation of the typeclass above 
+data Dict k v = -- ???
+
+instance {- (???) => -} IDict Dict k v where
+  -- ???
+
+
+-- 3. Implement the Show typeclass to represent your dictionary 
+--    in the form {key : value} 
+instance (Show k, Show v) => Show (Dict k v) where
+  show :: Dict k v -> String
+  show x = -- ???
+
+
+{- OPTIONAL
+-- 4. Implement the Fucntor typeclass
+--    that alows mapping over dictionary
+--    Uncomment this block, if you do this
+instance Functor (Dict k) where
+  fmap :: (v -> p) -> Dict k v -> Dict k p
+  fmap f dict = -- ???
+-}
+
+-- please, make sure your code runs without errors
+-- comment out unsolved tasks
+main = do
+  print $ fromPairs kvPairs
+  where
+    kvPairs   = [(1,'h'), (2,'e'), (3, 'l'), (4,'l'), (5, 'o')]
+    fromPairs = foldl insert' empty
+    insert' dict (k, v) = insert k v dict
